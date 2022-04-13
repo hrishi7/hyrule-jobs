8827f89f7fc1f13a691b739d16e93e16f43bbe81
Hrishikesh <hrishikeshbaidya7@gmail.com>
2022-04-13 19:00:49 +0530

    upload

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="title">
+        <img src="./assets/heart.svg" alt="logo" />
+        <h1>Hyrule jobs</h1>
+      </div>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+
+    <JobList :jobs="jobs" :order="order" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from "vue";
+import Job from "./types/Job";
+import OrderTerm from "./types/OrderTerm";
+import JobList from "@/components/JobList.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: "some person 1", location: "kolkata", salary: 7688, id: 1 },
+      { title: "some person 2", location: "delhi", salary: 2344, id: 2 },
+      { title: "some person 3", location: "mimbai", salary: 5677, id: 3 },
+      { title: "some person 4", location: "pune", salary: 12344, id: 4 },
+      { title: "some person 5", location: "bihar", salary: 1234, id: 5 },
+    ]);
+    const order = ref<OrderTerm>("title");
+    const handleClick = (term: OrderTerm) => {
+      order.value = term;
+    };
+    return { jobs, handleClick, order };
+  },
+
+  methods: {},
+});
+</script>
+
+<style scoped>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+header .title {
+  display: flex;
+  justify-content: center;
+}
+header img {
+  width: 60px;
+  margin-right: 20px;
+}
+header h1 {
+  font-size: 3em;
+}
+</style>
